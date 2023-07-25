e5e93efbdaacbd3f0e77cb9b4685afbcff891574
webdevmahdi <mahdihasan01705259@gmail.com>
2023-07-25 12:33:51 +0600

    third

--- js/app.js
+++ js/app.js
@@ -145,7 +145,7 @@ const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
-    document.getElementById("liked").appendChild(div);
+      document.getElementById("likedPost").appendChild(div);
   });
 };
 
